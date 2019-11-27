6aa45eaeaf5e3fe9c959b97819b7730ddd1b0309
Max Soe <58025767+bayzat-maxsoe@users.noreply.github.com>
2019-11-27 16:57:48 +0400

    Trying on gh-pages

---
+++ precache-manifest.9a80cc592130de30090fff82b5dec4ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84329a42271907e54d52f06255bd847",
+    "url": "/b2c-claims/index.html"
+  },
+  {
+    "revision": "b7ce2cb45259acfc2c9c",
+    "url": "/b2c-claims/static/css/main.7b429fbb.chunk.css"
+  },
+  {
+    "revision": "b8aa315b95f976b5873c",
+    "url": "/b2c-claims/static/js/2.18be540f.chunk.js"
+  },
+  {
+    "revision": "b7ce2cb45259acfc2c9c",
+    "url": "/b2c-claims/static/js/main.c7540c0e.chunk.js"
+  },
+  {
+    "revision": "853430ba77f8d907f99d",
+    "url": "/b2c-claims/static/js/runtime-main.eb7b7607.js"
+  },
+  {
+    "revision": "bd827ad2bfbc5b58d6edfa9153f8f8f3",
+    "url": "/b2c-claims/static/media/claimformexample.bd827ad2.png"
+  },
+  {
+    "revision": "21f97fdb4c9c4fff2bb480e9c1ac7028",
+    "url": "/b2c-claims/static/media/logo-bayzat-symbol--white.21f97fdb.svg"
+  },
+  {
+    "revision": "3d4dc0ade11c96bed00daf5e3c0c7401",
+    "url": "/b2c-claims/static/media/pdfpreview.3d4dc0ad.png"
+  }
+]);
